9d6547ec9ec004b78358e935da9a4e3a21fccc7b
q815438609 <815438609@qq.com>
2018-10-24 12:06:18 +0800

    aa

--- AA/js/myGame.js
+++ AA/js/myGame.js
@@ -1,23 +1,69 @@
-var randomNumber=Math.floor((Math.random()*101));
-var guessField=document.querySelector('.guessField');
+var randomNumber = Math.floor((Math.random() * 101));
+var guessField = document.querySelector('.guessField');
 
-var guessSubmit=document.querySelector('.guessSubmit');
-var guesses=document.querySelector('.guesses');
-var lastResult=document.querySelector('.lastResult');
-var lowOrHi=document.querySelector('.lowOrHi');
+var guessSubmit = document.querySelector('.guessSubmit');
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
 
-var guessCoutn=1;
-var resetButtom;
+var guessCount = 1;
+var resetButton;
 guessField.focus();
 
-function checkGuess()
-{
-    guesses.textContent += guessField.value+" ";
-    guesses.style.backgroundColor="yellow";
+function checkGuess() {
+    var userGuess = Number(guessField.value);
 
+    if (guessCount === 1) {
+        guesses.textContent = '上次猜的数'
+    }
+    guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = '恭喜你！猜对了！';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+    }
+    else if (guessCount === 10) {
+        lastResult.textContent = '!!!GAME OVER!!!';
+        lowOrHi.textContent = '';
+        setGameOver();
+    }
+    else {
+        lastResult.textContent = '你猜错了！';
+        lastResult.style.backgroundColor = 'red';
+
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = '刚才你猜低了!';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = '刚才你猜高了!';
+        }
+
+    }
+    guessCount++;
+
+    guessField.focus();
+    guesses.style.backgroundColor = "yellow";
 }
 
-guessSubmit.addEventListener('click',checkGuess);
+guessSubmit.addEventListener('click', checkGuess);
+
+// var resetButtonP=document.querySelector("div.resultParas p:last-child");
+// console.log(resetButtonP);
+// resetButtonP.style.display="block";
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+
+    resetButton=document.createElement('button');
+
+    resetButton.textContent='开始新游戏';
+
+    document.body.appendChild(resetButton);
+
+    resetButton.addEventListener('click',resetGame);
+}
+
 
 // checkGuess();
 
