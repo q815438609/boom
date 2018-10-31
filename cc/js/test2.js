8aefa88f278ce4ec6ee420f2ca1c5d609b28f045
q815438609 <815438609@qq.com>
2018-10-31 12:32:45 +0800

    aaa

--- cc/js/test2.js
+++ cc/js/test2.js
@@ -1,75 +1,106 @@
-// // css选择器--document.querySelector()
-// var img4=document.querySelector('#images a:nth-child(4)');
-// // ID--document.getElementByid()
+// CSS选择器--document.querySelector()
+// var img4=document.querySelector('#images>a:nth-child(4)');
+//ID--d
 // var img4=document.getElementById('images').children[4];
-// //类名--document.getElementsByClassName()
 // var img4=document.getElementsByClassName('hiddenImg')[3];
-// //标签名--document.getElementsByTagName()
-// var img4=document.getElementsByTagName('a')[4]; 
+// var img4=document.getElementsByName('a')[4];
 // console.log(img4);
 
-// img4.style.display='block'
+// img4.style.display="block";
+// img0.style.display="none";
 
-
-//获取一组带超链接的图像
+//获取一组带图像的超链接
 var imagesA=document.getElementById("images").children;
+//获取一组li文本
+var txtList=document.querySelectorAll(".txtItem");
 
-//获取一组文本
-var txtList=document.querySelectorAll(".txt-box>li")
-
-
-
-// console.log(imagesA);
-
-
-//通过更换CCSS类名来实现
-//26行隐藏
-// imagesA[0].style.display="none";
-// //30行显示
-// imagesA[4].style.display="block";
-
-//利用计时器间隔1s,显示1张图像，其余图像隐藏
+//初始化当前显示的图片/文本编号
 var currentNo=0;
-function changeImg()
-{
-//排他原理，先去掉同类，再突出自己
 
-  for(var i=0;i<imagesA.length;i++)
-  {
-      imagesA[i].className="hiddenImg";
-      txtList[i].className="txtItem normalColor";
+//计时器换片函数，间隔1s被调用，显示1张图像，其余图像隐藏。文本轮流高亮
+function changeImg(){
+    
+    var nodeLength=txtList.length
+    //排他原理1 ，将同类设置为统一状态
+    for(var i=0;i<nodeLength;i++){
+        // 同类图片透明度0(.hiddenImg)
+        imagesA[i].className="hiddenImg";
+        //同类文本设置正常非高亮
+        txtList[i].className="txtItem normalColor";
+        // console.log(imagesA[i]);
+    }
+    
 
-    //   console.log(imagesA[i]);
-  }
- 
-
-    //再突出自己
+    //排他原理2，突出自己，当前图片透明度1(.displayImg)3
     imagesA[currentNo].className="displayImg";
+    //排他原理2，文本高亮
     txtList[currentNo].className="txtItem heighlightColor";
 
-    //换个元素，为下一个计时器调用做准备
-    if(currentNo<7){currentNo++;}
-    else{currentNo=0;}
-
-    // console.log(currentNo);
-
+    //换个编号，为下一次计时器调用做准备
+    if(currentNo<7) {currentNo++;}
+    else{
+        currentNo=0;
+    }
+    console.log(currentNo);
 }
+
+//网页加载后启动定时器
 var timer=window.setInterval(changeImg,1000)
 
-//定时器移入移出暂停启动按钮
-
-function stopChange()
-{
-    window.clearInterval(timer)
+//鼠标移出后移除定时器
+function stopChange(){
+    window.clearInterval(timer);
 }
-function startChange()
-{
+//鼠标移入后重设定时器
+function startChange(){
     timer=window.setInterval(changeImg,1000);
 }
-var imagesDiv=document.getElementById("images");
-console.log(imagesDiv);
-imagesDiv.addEventListener('mouseover',stopChange);
-imagesDiv.addEventListener('mouseout',startChange);
+
+var sliderDiv=document.querySelector(".slider");
+// console.log(imagesDiv);
+sliderDiv.addEventListener('mouseover',stopChange);
+sliderDiv.addEventListener('mouseout',startChange);
 
 
+//为所有文本Li注册鼠标移入事件
+for(var i=0;i<txtList.length;i++){
+    txtList[i].addEventListener('mouseover',gotoImg);
 
+    //添加自定义属性no 记录当前li的编号
+    txtList[i].no=i;
+    // console.log(txtList[i].no);
+}
+//移入之后 当前li高亮，跳转到对应图片
+function gotoImg(){
+    // console.log(txtList[i]);
+    //获得当前显示图像的编号/文本编号 this 是当前事件发生的本体
+    console.log(this.no);
+    //调用更换图片/
+    currentNo=this.no;
+    changeImg();
+}
+
+var leftButton=document.querySelector('.leftButton');
+var rightButton=document.querySelector('.rightButton');
+// console.log(leftButton);
+
+leftButton.addEventListener('click',leftImg);
+rightButton.addEventListener('click',rightImg);
+
+function leftImg(){
+   
+    if(currentNo>0) {currentNo--;}
+    else{
+        currentNo=7;
+    }
+
+}
+
+
+function rightImg(){
+    if(currentNo < 7) {currentNo++;}
+    else{
+        currentNo=0;
+    }
+   
+}
